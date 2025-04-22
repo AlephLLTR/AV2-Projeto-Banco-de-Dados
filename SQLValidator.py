a8112c6457820d7894ae93dad525a698ed641ea8
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-04-22 13:00:53 -0300

    conversão sql para álgebra relacional

--- SQLValidator.py
+++ SQLValidator.py
@@ -23,3 +23,39 @@ class SQLValidator:
             return details
         else:
             return "Consulta inválida!"
+
+
+def to_relational_algebra(parsed):
+    if not parsed:
+        return "Consulta inválida"
+
+    # Projeção
+    projection = f"π_{parsed['select_clause']}"
+
+    # Tabela
+    base = parsed["from_clause"]
+
+    # JOIN
+    if parsed["joins"] and parsed["joins"][0] and parsed["joins"][1]:
+        join_table = parsed["joins"][0]  
+        join_condition = parsed["joins"][1] 
+        base = f"{base} ⨝_{join_condition} {join_table}"
+
+    # Seleção
+    if parsed["where_clause"]:
+        selection = f"σ_{parsed['where_clause']}({base})"
+        return f"{projection}({selection})"
+
+    return f"{projection}({base})"
+
+# AQUI ESSA MAIN AQUI É SÓ PRA TESTAR
+
+# if __name__ == "__main__":
+#     query = input("Digite a consulta SQL: ")
+
+#     validator = SQLValidator()
+#     parsed = validator.explain_validation(query)
+#     algebra = to_relational_algebra(parsed)
+
+#     print("\n parser:", parsed, "\n")
+#     print("Álgebra Relacional:", algebra, "\n")
